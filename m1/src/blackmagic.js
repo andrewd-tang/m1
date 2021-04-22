c5fb20f38ea83938ff88596d6b5bdaea9909169f
andrewd-tang <83037346+andrewd-tang@users.noreply.github.com>
2021-04-22 15:57:07 -0400

    Add files via upload

---
+++ m1/src/blackmagic.js
@@ -0,0 +1,325 @@
+
+/* Buttons clicks*/
+$(document).ready(function() {
+    $('#Space').click(function() {
+      text = text+' ';
+      $('#text_disp').html(text);
+    });
+    $('#Delete').click(function() {
+      text = text.slice(0, -1);
+      $('#text_disp').html(text);
+    });
+    $('#a').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+          text = text+'a';
+          $('#text_disp').html(text);
+          $('#keyboardV').css("display", "none");
+          $('#keyboardMenu').css("display", "block");
+        }
+    });
+    $('#b').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'b';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#c').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'c';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#d').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'d';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#e').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'e';
+        $('#text_disp').html(text);
+        $('#keyboardV').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#f').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'f';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#g').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'g';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#h').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'h';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#i').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'i';
+        $('#text_disp').html(text);
+        $('#keyboardV').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#j').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'j';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#k').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'k';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#l').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'l';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#m').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'m';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#n').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'n';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#o').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'o';
+        $('#text_disp').html(text);
+        $('#keyboardV').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#p').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'p';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#q').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'q';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#r').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'r';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#s').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'s';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#t').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'t';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#u').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'u';
+        $('#text_disp').html(text);
+        $('#keyboardV').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#v').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'v';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#w').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'w';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#x').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'x';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#y').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'y';
+        $('#text_disp').html(text);
+        $('#keyboardV').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#z').click(function() {
+      if ($(this).parent().hasClass('dragging')) {
+          $(this).parent().removeClass('dragging');
+      }
+      else {
+        text = text+'z';
+        $('#text_disp').html(text);
+        $('#keyboardC').css("display", "none");
+        $('#keyboardMenu').css("display", "block");
+      }
+    });
+    $('#Vowels').click(function() {
+      $('#keyboardMenu').css("display", "none");
+      $('#keyboardV').css("display", "block");
+    });
+    $('#Consonants').click(function() {
+      $('#keyboardMenu').css("display", "none");
+      $('#keyboardC').css("display", "block");
+    });
+});
+
+
+$('#keyboardC').draggable({
+  cancel:false,
+  axis: 'x',
+  containment: [-1024, 0, 0, 0],
+  start: function (event, ui) {
+      $(this).addClass('dragging');
+  }
+});
+
+$('#keyboardV').draggable({
+  cancel:false,
+  axis: 'x',
+  containment: [-128, 0, 0, 0],
+  start: function (event, ui) {
+      $(this).addClass('dragging');
+  }
+});
